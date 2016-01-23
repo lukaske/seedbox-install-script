71f6067931e08a20d7181f0cd6941696dbe47f4f
rex <rex@linux.com>
2016-01-22 23:50:16 -0900

    changed clone location

--- torrent-server.sh
+++ torrent-server.sh
@@ -2,7 +2,7 @@
 #!/bin/sh
 sudo mkdir /home/seedbox
 sudo adduser --disabled-password --system --home /home/seedbox/deluge --group seedbox
-sudo apt-get update  # To get the latest package lists
+#sudo apt-get update  # To get the latest package lists
 sudo apt-get install python deluged deluge-webui deluge-console -y
 sudo touch /var/log/deluged.log
 sudo touch /var/log/deluge-web.log
@@ -14,4 +14,4 @@ sudo su - seedbox -c deluge-console "config -s allow_remote True"
 sudo su - seedbox -c deluge-console "config allow_remote"
 sudo cp deluged.conf /etc/init/deluged.conf
 sudo cp deluge-web.conf /etc/init/deluge-web.conf
-git clone git://github.com/RuudBurger/CouchPotatoServer.git /home/seedbox/.couchpotato
+sudo git clone git://github.com/RuudBurger/CouchPotatoServer.git /home/seedbox/.couchpotato
