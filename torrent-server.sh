adc4fcc7d469ca941ad2e67ef5d381510f02c35a
rex <rex@linux.com>
2016-01-22 23:01:20 -0900

    updating uid:gid

--- torrent-server.sh
+++ torrent-server.sh
@@ -1,5 +1,7 @@
  
 #!/bin/sh
+sudo mkdir /home/seedbox
+sudo adduser --disabled-password --system --home /home/seedbox/deluge --group seedbox
 sudo useradd seedbox -m -s /bin/bash
 su --shell /bin/bash --login seedbox
 sudo apt-get update  # To get the latest package lists
@@ -7,7 +9,7 @@ sudo apt-get install python deluged deluge-webui deluge-console -y
 sudo touch /var/log/deluged.log
 sudo touch /var/log/deluge-web.log
 sudo chown seedbox:seedbox /var/log/deluge*
-echo seedbox:seedbox:10 >> ~/.config/deluge/auth
+echo seedbox:seedbox:10 >> /home/seedbox/.config/deluge/auth
 deluge-console "config -s allow_remote True"
 deluge-console "config allow_remote"
 
