ed43fbc303e7765293f8087add0a681dbe563b04
rex <rex@linux.com>
2016-01-22 23:36:49 -0900

    fixed deluge-console commands

--- torrent-server.sh
+++ torrent-server.sh
@@ -2,14 +2,16 @@
 #!/bin/sh
 sudo mkdir /home/seedbox
 sudo adduser --disabled-password --system --home /home/seedbox/deluge --group seedbox
-su --shell /bin/bash --login seedbox
 sudo apt-get update  # To get the latest package lists
 sudo apt-get install python deluged deluge-webui deluge-console -y
 sudo touch /var/log/deluged.log
 sudo touch /var/log/deluge-web.log
 sudo chown seedbox:seedbox /var/log/deluge*
-echo seedbox:seedbox:10 >> /home/seedbox/.config/deluge/auth
-deluge-console "config -s allow_remote True"
-deluge-console "config allow_remote"
-
+sudo mkdir /home/seedbox/.config
+sudo mkdir /home/seedbox/.config/deluge
+sudo cp auth /home/seedbox/.config/deluge
+sudo su - seedbox -c deluge-console "config -s allow_remote True"
+sudo su - seedbox -c deluge-console "config allow_remote"
+sudo cp deluged.conf /etc/init/deluged.conf
+sudo cp deluge-web.conf /etc/init/deluge-web.conf
 git clone git://github.com/RuudBurger/CouchPotatoServer.git .couchpotato
