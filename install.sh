e045d273319440ef4c6d953928c7574d14fe8f1f
rex <rex@linux.com>
2016-01-24 07:16:19 -0900

    fixed permissions

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ sudo adduser --disabled-password --system --home /home/seedbox --group seedbox
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
 echo "deb http://apt.sonarr.tv/ master main" | sudo tee /etc/apt/sources.list.d/sonarr.list
 sudo apt-get update 
-sudo apt-get install python deluged deluge-webui deluge-console -y
+sudo apt-get install python deluged deluge-webui deluge-console nzbdrone -y
 sudo touch /var/log/deluged.log
 sudo touch /var/log/deluge-web.log
 sudo chown seedbox:seedbox /var/log/deluge*
+sudo chmod -R 777 /home/seedbox
 sudo mkdir /home/seedbox/.config
 sudo mkdir /home/seedbox/.config/deluge
 sudo cp auth /home/seedbox/.config/deluge
@@ -22,6 +23,5 @@ sudo chmod +x /etc/init.d/couchpotato
 sudo cp ubuntu.default /etc/default/couchpotato
 sudo chmod +x /etc/default/couchpotato
 sudo update-rc.d couchpotato defaults
-sudo apt-get install nzbdrone
 sudo cp nzbdrone.conf /etc/init/
 sudo reboot now
