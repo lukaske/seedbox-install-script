5a67f3901a1524b6f02c3882a2c6e47781ad0aa7
rex <rex@linux.com>
2016-01-27 13:01:00 -0900

    fixing deluge remote config

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ sudo su - seedbox -c deluge-console "config -s allow_remote True"
 sudo su - seedbox -c deluge-console "config allow_remote"
 sudo cp deluged.conf /etc/init/deluged.conf
 sudo cp deluge-web.conf /etc/init/deluge-web.conf
-sudo rm /home/seedbox/.config/deluge/auth
-sudo cp auth /home/seedbox/.config/deluge
+sudo echo "seedbox:seedbox:10" >> /home/seedbox/.config/deluge/auth
+#sudo cp auth /home/seedbox/.config/deluge
 sudo mkdir /home/seedbox/.couchpotato
 sudo git clone git://github.com/RuudBurger/CouchPotatoServer.git /home/seedbox/.couchpotato
 sudo cp /home/seedbox/.couchpotato/init/ubuntu /etc/init.d/couchpotato
